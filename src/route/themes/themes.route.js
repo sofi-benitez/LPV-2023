fb03497b91fa76ecfbb2d1695fff1b5bd45f54c8
sofi-benitez <42775504+sofi-benitez@users.noreply.github.com>
2023-03-29 10:06:17 -0400

    agg themes_properties

--- src/route/themes/themes.route.js
+++ src/route/themes/themes.route.js
@@ -1,4 +1,5 @@
 const themesController = require('../../controller/themes/themes.controller');
+const themesPropietiesController = require('../../controller/themes/themes-properties.controller');
 
 module.exports = function(app) {
 
@@ -7,4 +8,8 @@ module.exports = function(app) {
     app.post("/themes/update", themesController.actualizar);
     app.delete("/themes/delete/:id", themesController.eliminar);
     
+    app.get("/themes-properties/list", themesPropietiesController.listar);
+    app.get("/theme-propertie/:id", themesPropietiesController.Consultarid);
+    app.post("/themes-properties/update", themesPropietiesController.actualizar);
+    app.delete("/themes-properties/delete/:id", themesPropietiesController.eliminar);
 }
